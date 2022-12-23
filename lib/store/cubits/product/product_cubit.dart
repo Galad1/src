ce56dd19852b25e5b064d58b639996a6aa78524f
Galad1 <57092458+Galad1@users.noreply.github.com>
2022-12-23 09:52:33 +0100

    changes

--- lib/store/cubits/product/product_cubit.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class ProductCounter extends Cubit<int> {
-  ProductCounter() : super(0);
-
-  void incrementProductCounter() => emit(state + 1);
-}
